f0a157abcdf2269055c0569485b11bb806b183c4
qywang6 <1041745835@qq.com>
2019-01-16 21:52:31 +0800

    测试

--- src/com/wqy/student/utils/CommonUtils.java
+++ src/com/wqy/student/utils/CommonUtils.java
@@ -3,6 +3,7 @@ package com.wqy.student.utils;
 import java.util.Scanner;
 
 public class CommonUtils {
+	
 	private static Scanner in =new Scanner(System.in);
 
 	public static Scanner getScanner() {
